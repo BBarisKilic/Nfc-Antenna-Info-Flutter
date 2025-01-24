f6283514f909d5c97276530bea01e4afffc601bd
Bulent Baris Kilic <bulentbariskilic@gmail.com>
2025-01-25 00:53:14 +0100

    ci: bump gradle and agp versions to fix pipeline issues

--- nfc_antenna_info_flutter/example/integration_test/app_test.dart
+++ nfc_antenna_info_flutter/example/integration_test/app_test.dart
@@ -1,8 +1,8 @@
 import 'dart:io';
 
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
-
 import 'package:nfc_antenna_info_flutter_example/main.dart' as app;
 
 void main() {
@@ -14,8 +14,15 @@ void main() {
       await tester.pumpAndSettle();
       await tester.tap(find.text('Get Platform Name'));
       await tester.pumpAndSettle();
-      final expected = expectedPlatformName();
-      await tester.ensureVisible(find.text('Platform Name: $expected'));
+      final expectedPlatform = expectedPlatformName();
+      final expectedWidget = find.byWidgetPredicate((widget) {
+        return switch (widget) {
+          Text(data: final String text) =>
+            text.startsWith('Platform Name: $expectedPlatform'),
+          _ => false,
+        };
+      });
+      await tester.ensureVisible(expectedWidget);
     });
   });
 }
