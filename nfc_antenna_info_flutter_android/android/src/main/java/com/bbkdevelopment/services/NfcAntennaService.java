70c0dc2d55f2d571346313ca7c485c585b598316
Bulent Baris Kilic <bulentbariskilic@gmail.com>
2025-01-27 19:32:01 +0100

    feat: adjust android plugin to retrieve mock data when it runs on emulator

--- nfc_antenna_info_flutter_android/android/src/main/java/com/bbkdevelopment/services/NfcAntennaService.java
+++ nfc_antenna_info_flutter_android/android/src/main/java/com/bbkdevelopment/services/NfcAntennaService.java
@@ -1,6 +1,7 @@
 package com.bbkdevelopment.services;
 
 import android.content.Context;
+import android.nfc.AvailableNfcAntenna;
 import android.nfc.NfcAdapter;
 import android.nfc.NfcAntennaInfo;
 import android.os.Build;
@@ -8,6 +9,8 @@ import com.bbkdevelopment.exceptions.NfcAntennaInfoFlutterPluginException;
 import com.bbkdevelopment.exceptions.NfcDisabledException;
 import com.bbkdevelopment.exceptions.NfcUnavailableException;
 import com.bbkdevelopment.exceptions.UnsupportedFeatureException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A service to retrieve NFC antenna information on Android devices. This service provides methods
@@ -30,6 +33,11 @@ public final class NfcAntennaService {
       throw new UnsupportedFeatureException();
     }
 
+    // Check if the device is an emulator and return a mock NFC antenna information object.
+    if (isEmulator()) {
+      return getMockNfcAntennaInfo();
+    }
+
     try {
       final NfcAdapter nfcAdapter = NfcAdapter.getDefaultAdapter(context);
       if (nfcAdapter == null) {
@@ -44,5 +52,41 @@ public final class NfcAntennaService {
     } catch (Exception exception) {
       throw new NfcUnavailableException();
     }
+  }
+
+  /**
+   * Checks if the device is an emulator.
+   *
+   * @return True if the device is an emulator, otherwise false.
+   */
+  private Boolean isEmulator() {
+    return (Build.BRAND.startsWith("generic") && Build.DEVICE.startsWith("generic"))
+        || Build.FINGERPRINT.startsWith("generic")
+        || Build.FINGERPRINT.startsWith("unknown")
+        || Build.HARDWARE.contains("goldfish")
+        || Build.HARDWARE.contains("ranchu")
+        || Build.MODEL.contains("google_sdk")
+        || Build.MODEL.contains("Emulator")
+        || Build.MODEL.contains("Android SDK built for x86")
+        || Build.MANUFACTURER.contains("Genymotion")
+        || Build.PRODUCT.contains("sdk")
+        || Build.PRODUCT.contains("vbox86p")
+        || Build.PRODUCT.contains("emulator")
+        || Build.PRODUCT.contains("simulator");
+  }
+
+  /**
+   * Retrieves a mock NFC antenna information object for testing purposes.
+   *
+   * @return A mock NFC antenna information object.
+   */
+  private NfcAntennaInfo getMockNfcAntennaInfo() {
+    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+      throw new UnsupportedFeatureException();
+    }
+
+    final List<AvailableNfcAntenna> mockAvailableNfcAntennas = new ArrayList<>();
+    mockAvailableNfcAntennas.add(new AvailableNfcAntenna(33, 97));
+    return new NfcAntennaInfo(68, 136, false, mockAvailableNfcAntennas);
   }
 }
