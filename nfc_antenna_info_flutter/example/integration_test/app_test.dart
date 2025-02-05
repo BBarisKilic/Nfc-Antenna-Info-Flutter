278afe4b0244e82bb5218e4ab631559e2f07dc32
Bulent Baris Kilic <bulentbariskilic@gmail.com>
2025-02-05 23:54:04 +0100

    test: add proper integration test for getNfcAntennaInfo

--- nfc_antenna_info_flutter/example/integration_test/app_test.dart
+++ nfc_antenna_info_flutter/example/integration_test/app_test.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
@@ -9,25 +7,14 @@ void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
   group('E2E', () {
-    testWidgets('getPlatformName', (tester) async {
+    testWidgets('getNfcAntennaInfo', (tester) async {
       app.main();
-      await tester.pumpAndSettle();
-      await tester.tap(find.text('Get Platform Name'));
-      await tester.pumpAndSettle();
-      final expectedPlatform = expectedPlatformName();
-      final expectedWidget = find.byWidgetPredicate((widget) {
-        return switch (widget) {
-          Text(data: final String text) =>
-            text.startsWith('Platform Name: $expectedPlatform'),
-          _ => false,
-        };
-      });
+      final expectedWidget = find.byIcon(Icons.nfc);
+      await tester.pumpFrames(
+        const app.MyApp(),
+        const Duration(seconds: 1),
+      );
       await tester.ensureVisible(expectedWidget);
     });
   });
-}
-
-String expectedPlatformName() {
-  if (Platform.isAndroid) return 'Android';
-  throw UnsupportedError('Unsupported platform ${Platform.operatingSystem}');
 }
